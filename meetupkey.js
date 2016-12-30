026e60e65f2faf8ac5522dc007fb93196a35ad62
Kevin Chau <kchau.jobs@gmail.com>
2016-12-29 18:52:24 -0800

    return json identical to previous json file

--- meetupkey.js
+++ meetupkey.js
@@ -3,3 +3,4 @@
  */
 var meetUpKey1 = '702403fb782d606165f7638a242a';
 var meetUpKey2 = '163736143b31146c5361736d41103459';
+var youtubeKeu = 'AIzaSyBvpxdfij0AHndbeKuWQSGhcgJoJ6B-xNo';
---
+++ youtube/getVideos.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 12/29/16
+ * Time: 5:53 PM
+ */
+require_once('../meetupkeys.php');
+
+$output = [
+    'success'=>false
+];
+
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);
+header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+
+//print_r($_GET["userQuery"]);
+$searchQuery = $_GET["userQuery"];
+
+if($youtubeData = file_get_contents("https://www.googleapis.com/youtube/v3/search?part=snippet&key=$youtubeKey&q=$searchQuery&maxResults=10&type=video&videoDefinition=high", false, stream_context_create($arrContextOptions))){
+    $decoded = json_decode($youtubeData);
+    //print_r($decoded->items[0]->snippet);
+
+    $videos = [];
+
+    foreach($decoded->items as $videoItem){
+        $videos[] =[
+            'title'=>$videoItem->snippet->title,
+            'id'=>$videoItem->id->videoId
+        ];
+    }
+    //print_r($output);
+    $output=[
+        'success'=>true,
+        'video'=>$videos
+    ];
+}
+
+print_r(json_encode($output));
