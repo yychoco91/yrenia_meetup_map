f1bb0d81cc5965d230f25539e12c00c01cc5d1db
Kevin Chau <kchau.jobs@gmail.com>
2016-12-29 19:06:48 -0800

    edited config for youtube

--- apikeys.php
+++ apikeys.php
@@ -3,3 +3,4 @@
 $googleKey = 'AIzaSyDa6lkpC-bOxXWEbrWaPlw_FneCpQhlgNE';
 $meetupKey1 = '702403fb782d606165f7638a242a';
 $meetupKey2 = '163736143b31146c5361736d41103459';
+$youtubeKey = 'AIzaSyCabtZUadHeQsAEFpZyD5cwoz2RKMmwWpg';
